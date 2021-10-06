a80386b6eb7380823afd14eab4f61d0a653382ca
rafao <rcsva_sousa@hotmail.com>
2021-10-06 01:12:15 -0300

    add behavior

--- src/App.js
+++ src/App.js
@@ -9,9 +9,67 @@ class App extends Component {
       <Typography variant="h3" component="h1" align="center">
         Sign-up Form
       </Typography>
-      <SignUpForm />
+      <SignUpForm signUp={signUp} validateCPF={validateCPF} />
     </Container>;
   }
+}
+
+function signUp(data) {
+  console.log(data);
+}
+
+function validateCPF(cpf) {
+  const result = { valid: true, message: "" };
+
+  if (cpf.length !== 11) {
+    result.valid = false;
+    result.message = "CPF must have 11 characters."
+    return result;
+  }
+
+  if (!tryCPF(cpf)) {
+    result.valid = false;
+    result.message = "Please enter a valid CPF."
+    return result;
+  }
+
+  return result;
+}
+
+function tryCPF(cpf) {
+  if (cpf === "00000000000") {
+    return false;
+  }
+
+  if (_validateCpfChunk(cpf, 9, 11) !== parseInt(cpf.substring(9, 10))) {
+    return false;
+  }
+
+  if (_validateCpfChunk(cpf, 10, 12) !== parseInt(cpf.substring(10, 11))) {
+    return false;
+  }
+
+  return true;
+}
+
+function _validateCpfChunk(cpf, length, aux) {
+  let sum = 0;
+
+  for (let i = 1; i <= length; i++) {
+    sum = sum + parseInt(cpf.substring(i - 1, i)) * (aux - i);
+  }
+
+  return _validateRest(sum);
+}
+
+function _validateRest(sum) {
+  const rest = (sum * 10) % 11;
+
+  if ((rest === 10) || (rest === 11)) {
+    return 0;
+  }
+
+  return rest;
 }
 
 export default App;
--- src/components/signUpForm/SignUpForm.jsx
+++ src/components/signUpForm/SignUpForm.jsx
@@ -1,12 +1,31 @@
+import React, { useState } from "react";
 import { Button, FormControlLabel, Switch, TextField } from "@mui/material";
 
-function SignUpForm() {
+function SignUpForm({ signUp, validateCPF }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [cpf, setCPF] = useState("");
+  const [sales, setSales] = useState(true);
+  const [news, setNews] = useState(true);
+  const [error, setError] = useState({ cpf: { valid: true, message: "" } });
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        signUp({ firstName, lastName, cpf, sales, news });
+      }}
+    >
       <TextField
         id="firstName"
         label="First name"
         margin="normal"
+        onChange={(event) => {
+          event.stopPropagation();
+          setFirstName(event.target.value);
+        }}
+        value={firstName}
         variant="outlined"
         fullWidth
       />
@@ -14,22 +33,57 @@ function SignUpForm() {
         id="lastName"
         label="Last name"
         margin="normal"
+        onChange={(event) => {
+          event.stopPropagation();
+          setLastName(event.target.value);
+        }}
+        value={lastName}
         variant="outlined"
         fullWidth
       />
       <TextField
         id="cpf"
+        error={!error.cpf.valid}
+        helperText={error.cpf.message}
         label="CPF"
         margin="normal"
+        onBlur={(event) => {
+          event.stopPropagation();
+          const result = validateCPF(cpf);
+          setError({ cpf: result });
+        }}
+        onChange={(event) => {
+          event.stopPropagation();
+          setCPF(event.target.value);
+        }}
+        value={cpf}
         variant="outlined"
         fullWidth
       />
       <FormControlLabel
-        control={<Switch id="sales" defaultChecked />}
+        control={
+          <Switch
+            id="sales"
+            checked={sales}
+            onChange={(event) => {
+              event.stopPropagation();
+              setSales(event.target.checked);
+            }}
+          />
+        }
         label="Sales"
       />
       <FormControlLabel
-        control={<Switch id="news" defaultChecked />}
+        control={
+          <Switch
+            id="news"
+            checked={news}
+            onChange={(event) => {
+              event.stopPropagation();
+              setNews(event.target.checked);
+            }}
+          />
+        }
         label="News"
       />
       <Button type="submit" variant="contained" color="primary">
